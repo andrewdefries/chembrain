fd9d7e865324abf50830397079a805afaf9fbbef
Andy Hayden <andyhayden1@gmail.com>
2013-11-09 02:33:42 -0800

    FIX molecule not molecule typo

--- molecule_converters/smi_to_chembrain.py
+++ molecule_converters/smi_to_chembrain.py
@@ -4,17 +4,19 @@ import openbabel as ob
 from StringIO import StringIO
 
 def smi_to_chembrain(smi_repr, from_format='smi'):
+    if not smi_repr:
+        return {}
     mol = pybel.readstring(from_format, smi_repr)
     mol.addh()
     df = pd.read_csv(StringIO(mol.write(format='ct')),
                         skiprows=len(mol.atoms) + 2,
                         sep='\s+', header=None)
 
-    molecules = [a.atomicnum for a in mol.atoms]
+    molecule = [a.atomicnum for a in mol.atoms]
 
     links = [list(row.iloc[[0, 1]] - 1)
                 for _, row in df.iterrows()]
 
-    return {"molecules": molecules, "links": links}
+    return {"molecule": molecule, "links": links}
 
 
