6cf65a02f89eb7055f57cb73874a9bff2b590914
Jon Parker <jparker165@gmail.com>
2013-11-09 01:08:07 -0800

    can only kill edge atoms

--- d3/scripts.js
+++ d3/scripts.js
@@ -1,53 +1,98 @@
 $(function() {
   initButtons()
 
-  nodes = []
-  links = []
-  selectedNode = null
+  link = d3.selectAll('.link')
+  node = d3.selectAll('.node')
+
+  var nodes = []
+  var links = []
   fill = d3.scale.category20()
 
   vis = d3.select('#main').append("svg")
                            .attr("width", '100%')
                            .attr("height", '100%')
 
-  force = d3.layout.force()
-         .nodes(nodes)
-         .links(links)
-         .charge(-1500)
-         .friction(0.3)
-         .gravity(0.1)
-         .size([500,500])
-         .start()
+  initForce(nodes, links)
 
   addAtom('C')
-
+  selectedNode = force.nodes()[0]
+  $('circle').attr('stroke', 'green');
 })
 
+function initForce(nodes, links) {
+  force = d3.layout.force()
+       .nodes(nodes)
+       .links(links)
+       .charge(-1500)
+       .friction(0.3)
+       .gravity(0.1)
+       .size([500,500])
+       .start()
+}
+
+// function start() {
+//   link = link.data(force.links(), function(d) { return d.source.id + "-" + d.target.id; });
+//   link.enter().insert("line", ".node").attr("class", "link");
+//   link.exit().remove();
+
+//   node = node.data(force.nodes(), function(d) { return d.id;});
+//   node.enter().append("circle")
+//     .attr("class", function(d) { return "node " + d.id; }).attr("r", 8)
+//     .attr("r", function(d) {
+//         return Math.pow(40 * d.size, 1/3);
+//     })
+//     .attr("fill", function(d) {
+//       return fill(d.size);
+//     })
+//     .attr("stroke", "black")
+//     .attr("stroke-width",2)
+//     .on('click', function(d,i) {
+//       $('circle').attr('stroke', 'black');
+//       $(this).attr('stroke', 'green');
+//       selectedNode = d;
+//     });
+
+//   node.enter().append("text")
+//             .attr("class", "nodetext")
+//             .attr("dx", 12)
+//             .attr("dy", ".35em")
+//             .text(function(d) {return d.atom});
+
+//   node.exit().remove();
+
+//   force.start();
+// }
+
+function hasUnpaired(atom) {
+  return _.filter(force.links(), function(link) {
+    return link.source.id == atom.id || link.target.id == atom.id
+  }).length < atom.electrons
+}
+
 function addAtom(element, linkedAtom) {
-  if (linkedAtom && linkedAtom.free == 0) {
+  if (linkedAtom && !hasUnpaired(linkedAtom)) {
+    console.log('WARNING: tried to link without available electron')
     return null
   }
 
   var atomRepr = atom(element)
-  atomRepr.id = nodes.length
-  nodes.push(atomRepr)
+  atomRepr.id = force.nodes().length
+  force.nodes().push(atomRepr)
 
   if (typeof linkedAtom !== 'undefined') {
-    links.push({source: atomRepr.id, target: linkedAtom.id, value: 1})
-    atomRepr.free -= 1;
-    linkedAtom.free -= 1;
+    force.links().push({source: atomRepr.id, target: linkedAtom.id, value: 1})
   }
 
   var link = vis.selectAll("line.link")
-    .data(links, function(d) { return d.source.id + "-" + d.target.id; });
+    .data(force.links(), function(d) { return d.source.id + "-" + d.target.id; });
 
   link.enter().insert("line")
       .attr("class", "link");
 
-  // link.exit().remove();
+  link.exit().remove();
 
   var node = vis.selectAll("g.node")
-    .data(nodes, function(d) { return d.id;});
+    .data(force.nodes(), function(d) { return d.id;});
 
   var nodeEnter = node.enter().append("g")
                               .attr("class", "node")
@@ -74,7 +119,7 @@ function addAtom(element, linkedAtom) {
             .attr("dy", ".35em")
             .text(function(d) {return d.atom});
 
-  // node.exit().remove();
+  node.exit().remove();
 
   force.on("tick", function() {
     link.attr("x1", function(d) { return d.source.x; })
@@ -93,16 +138,51 @@ function atom(letter) {
   switch(letter)
   {
     case "C":
-      return {"atom":"C", "size":12, free: 4};
+      return {atom:"C", size:12, electrons: 4};
     case "H":
-      return {"atom":"H", "size":1, free: 1};
+      return {atom:"H", size:1, electrons: 1};
     case "N":
-      return {"atom":"N", "size":7, free: 3};
+      return {atom:"N", size:7, electrons: 3};
     case "O":
-      return {"atom":"O", "size":16, free: 2};
+      return {atom:"O", size:16, electrons: 2};
     default:
       return {}; //Not sure what to do here...
   }
+}
+
+function connectedIds(atomId) {
+  var links = _.filter(force.links(), function(link) {
+    return link.target.id == atomId || link.source.id == atomId
+  })
+
+  var all_ids = _.map(links, function(link) {return [link.source.id, link.target.id]})
+  return _.uniq(_.reject(_.flatten(all_ids), function(id) { return id == atomId}))
+}
+
+function killAtom(atom) {
+  if (connectedIds(atom.id).length > 1) {
+    console.log('WARNING: removing non-edge atom')
+    return null
+  }
+
+  var nodes = _.reject(force.nodes(), function(node) {return node.id == atom.id});
+  var links = _.reject(force.links(), function(link) {
+    return link.source.id == atom.id || link.target.id == atom.id
+  })
+
+  initForce(nodes, links)
+
+  var node = vis.selectAll(".node");
+  node = node.data(nodes, function(d) { return d.id;});
+  node.exit().remove();
+
+  var link = vis.selectAll(".link");
+  link = link.data(links, function(d) { return d.source.id + "-" + d.target.id; });
+  link.exit().remove();
+
+  force.start();
+
+  selectedNode = null
 }
 
 function initButtons() {
@@ -110,7 +190,20 @@ function initButtons() {
     var element = $(this).attr('data-element')
     if (selectedNode) {
       addAtom(element, selectedNode);
+    } else if (force.nodes().length == 0) {
+      addAtom(element)
     }
+  });
+
+  $('.kill-selected').click(function(e) {
+    if (selectedNode) { killAtom(selectedNode); }
+  })
+
+  $('.clear').click(function(e) {
+    vis.selectAll('.node').remove()
+    vis.selectAll('.link').remove()
+    nodes = []
+    links = []
   })
 }
 
