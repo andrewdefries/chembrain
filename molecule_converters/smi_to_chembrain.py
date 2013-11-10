74d0c991cc3790087ff0925f51bb86a4d95680e2
Andy Hayden <andyhayden1@gmail.com>
2013-11-09 23:55:26 -0800

    more molecules in the json

--- molecule_converters/smi_to_chembrain.py
+++ molecule_converters/smi_to_chembrain.py
@@ -108,7 +108,7 @@ def smi_to_chembrain(smi_repr, from_format='smi'):
 
     molecule = [a.atomicnum for a in mol.atoms]
 
-    links = [list(row.iloc[[0, 1]])
+    links = [[int(i) for i in row.iloc[[0, 1]]]
                 for _, row in df.iterrows()]
 
     return {"nodes": [{"atomicNumber": molecule, "id": i}
