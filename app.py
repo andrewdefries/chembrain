1080739d9ec20956039ea57fe9628ede30d6a0d9
Jon Parker <jparker165@gmail.com>
2013-11-10 09:19:08 -0800

    base flask app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def webprint():
+    return render_template('base.html')
+
+if __name__ == '__main__':
+    app.run(port=8005, debug=True)
